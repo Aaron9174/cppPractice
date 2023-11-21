9f706e8234244b3f312a5a7c0b7cdec31f75b697
Aaron Hebson <Aarommh7@gmail.com>
2023-11-20 22:14:54 -0500

    Added new scripts for building c++ files easier

---
+++ c++/Section10_Characters_Strings/cStyleStrings/cstyle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    char first_name[20]{};
+    char last_name[20]{};
+    char full_name[50]{};
+    char temp[50]{};
+
+    cout << first_name; // Should display garbage
+
+    // cout << "Please enter your first name: ";
+    // cin >> first_name;
+
+    // cout << "Please enter your last name: ";
+    // cin >> last_name;
+
+    // cout << "Hello, " << first_name;
+    // cout << "and your last name is " << last_name;
+}
